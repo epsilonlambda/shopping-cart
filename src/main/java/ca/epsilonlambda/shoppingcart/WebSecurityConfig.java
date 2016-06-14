a9ac5b7b97ffff3ea4d61c16383a1ad26b66417e
kirill <kirill.epsilonlambda@gmail.com>
2016-06-14 03:16:49 -0400

    [ET-33] Implement JWT filter that decrypts the user id.
    * Add security filter chains to web config

--- src/main/java/ca/epsilonlambda/shoppingcart/WebSecurityConfig.java
+++ src/main/java/ca/epsilonlambda/shoppingcart/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package ca.epsilonlambda.shoppingcart;
 
+import ca.epsilonlambda.shoppingcart.security.UserTokenAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +19,13 @@ import org.springframework.stereotype.Service;
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserTokenAuthenticationFilter tokenAuthenticationFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.anonymous().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/", "/api/v1/rpc/anonymous_login").anonymous().and().csrf().disable()
+                .authorizeRequests().antMatchers("/api/v1/**").authenticated().and().addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
 }
--- src/test/java/ca/epsilonlambda/shoppingcart/integration/ProductsControllerIntegrationTest.java
+++ src/test/java/ca/epsilonlambda/shoppingcart/integration/ProductsControllerIntegrationTest.java
@@ -3,16 +3,18 @@ package ca.epsilonlambda.shoppingcart.integration;
 import ca.epsilonlambda.shoppingcart.Runner;
 import ca.epsilonlambda.shoppingcart.domain.Product;
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Collection;
 
 /**
  * Created by Kirill on 6/11/2016.
@@ -29,9 +31,19 @@ public class ProductsControllerIntegrationTest extends TestCase {
     @Value("${local.server.port}")
     int port;
 
+    HttpEntity<String> emptyRequestEntity;
+
+    @Before
+    public void setUp() {
+        String token = restTemplate.postForEntity("http://localhost:" + port + "/api/v1/rpc/anonymous_login", null, String.class).getBody();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+        emptyRequestEntity = new HttpEntity<>(headers);
+    }
+
     @Test
     public void testGet() {
-        Product[] products = restTemplate.getForEntity("http://localhost:" + port + "/api/v1/products", Product[].class).getBody();
+        Product[] products = restTemplate.exchange("http://localhost:" + port + "/api/v1/products", HttpMethod.GET, emptyRequestEntity, Product[].class).getBody();
         assertTrue(products.length > 0);
     }
 

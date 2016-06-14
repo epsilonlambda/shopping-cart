a49f93e016e2faac2b43f1667d48262f355f0c64
kirill <kirill.epsilonlambda@gmail.com>
2016-06-14 04:40:45 -0400

    [ET-36] Add API requester with JWT header support

---
+++ ui/src/main/js/helpers/apiRequester.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+import cookieDough from 'cookie-dough';
+
+export function getToken() {
+        var cookie = cookieDough();
+        return cookie.get('jwt');
+}
+
+export function apiRequest({path, method, success, fail, authenticate}) {
+    $.ajax({
+        method,
+        url: 'http://localhost:8080' + path,
+        headers: authenticate ? { 'Authorization': 'Bearer ' + getToken() } : undefined
+    }).done(success).fail(fail);
+}
+
+export function tokenRequest({path, method, success, fail}) {
+    $.ajax({
+        method,
+        url: 'http://localhost:8080' + path
+    }).done((data) => {
+        var cookie = cookieDough();
+        cookie.set('jwt', data, { maxAge: 31536e3 });
+        success();
+    }).fail(fail);
+}

bb50c8e3ef031fade353a1829450a8f0030af3fd
kirill <kirill.epsilonlambda@gmail.com>
2016-06-14 23:02:08 -0400

    [ET-40] Add repository to keep track of ordered items per customer

---
+++ src/main/java/ca/epsilonlambda/shoppingcart/domain/OrderItem.java
@@ -0,0 +1,42 @@
+package ca.epsilonlambda.shoppingcart.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by Kirill on 6/14/2016.
+ */
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @OneToOne
+    private User owner;
+
+    @OneToOne
+    private Product product;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    public OrderItem() {}
+
+    public OrderItem(User owner, Product product, int quantity) {
+        this.owner = owner;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}

90fdb079dfe6ec91e24914aaf8d47a01835be4f4
kirill <kirill.epsilonlambda@gmail.com>
2016-06-16 02:47:13 -0400

    [ET-49] Allow changing order quantity

--- ui/src/main/js/components/orderItemTile/actionCreators.js
+++ ui/src/main/js/components/orderItemTile/actionCreators.js
@@ -1,9 +1,26 @@
 import orderedItemsRepo from '../../orderedProductsRepository.js'
+import * as apiRequester from '../../helpers/apiRequester';
+import orderedProductRepo from '../../orderedProductsRepository.js';
 
 function deleteItem(productId) {
     return orderedItemsRepo.actionCreators.remove(productId);
 }
 
+function changeItemQuantity(product, quantity){
+    return (dispatch) => {
+        apiRequester.apiRequest({path: '/api/v1/me', authenticate: true, success: (user) => {
+            const requestBody = {
+                product,
+                owner: user,
+                quantity
+            };
+
+            dispatch(orderedProductRepo.actionCreators.update(product.id, requestBody));
+        }});
+    };
+}
+
 export default {
-    deleteItem
+    deleteItem,
+    changeItemQuantity
 };
